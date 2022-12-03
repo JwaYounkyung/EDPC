df4347c8c80f0e8d1f93fc69ec8cbc7ef27b025c
Minseon-Gwak <minseon25@postech.ac.kr>
2022-12-03 06:52:34 +0000

    feat: p_train update

--- EPC/maddpg_o/maddpg_local/micro/rmaddpg.py
+++ EPC/maddpg_o/maddpg_local/micro/rmaddpg.py
@@ -28,7 +28,7 @@ def make_update_exp(vals, target_vals):
     expression = tf.group(*expression)
     return U.function([], [], updates=[expression])
 
-def p_train(make_obs_ph_n, act_space_n, p_index, p_func, q_func, optimizer, grad_norm_clipping=None, local_q_func=False, num_units=64, scope="trainer", reuse=None):
+def p_train(make_obs_ph_n, act_space_n, p_index, p_func, p_nature_func, q_func, optimizer, optimizer_nature, grad_norm_clipping=None, local_q_func=False, num_units=64, scope="trainer", reuse=None):
     with tf.variable_scope(scope, reuse=reuse):
         # print(scope, p_index)
         # create distribtuions
@@ -52,10 +52,13 @@ def p_train(make_obs_ph_n, act_space_n, p_index, p_func, q_func, optimizer, grad
         act_ph_n = [act_pdtype_n[i].sample_placeholder([None], name="action"+str(i)) for i in range(len(act_space_n))]
 
         p_input = obs_ph_n[p_index]
+        p_nature_input = tf.concat(obs_ph_n + act_ph_n, 1) # not sure...
 
         # print("p_train/p_func:", scope)
         p = p_func(p_input, int(act_pdtype_n[p_index].param_shape()[0]), scope="p_func", num_units=num_units)
         p_func_vars = U.scope_vars(U.absolute_scope_name("p_func"))
+        p_nature = p_nature_func(p_nature_input, 1, scope="p_nature_func", num_units=num_units)
+        p_nature_func_vars = U.scope_vars(U.absolute_scope_name("p_nature_func"))
 
         # wrap parameters in distribution
         act_pd = act_pdtype_n[p_index].pdfromflat(p)
@@ -72,11 +75,20 @@ def p_train(make_obs_ph_n, act_space_n, p_index, p_func, q_func, optimizer, grad
         pg_loss = -tf.reduce_mean(q)
 
         loss = pg_loss + p_reg * 1e-3
+        
+        ### R-MADDPG loss ###
+        mse_weight = 1.0
+        perturbed_rewards = tf.placeholder(tf.float32, [None], name="perturbed") # placeholder for perturbed_rewards
+        pred_rewards = p_nature_func(p_nature_input, 1, scope="p_nature_func", num_units=num_units)[:,0]
+        loss_nature = tf.reduce_mean(pred_rewards) + mse_weight * tf.reduce_mean(tf.square(pred_rewards - perturbed_rewards))
+        #####################
 
         optimize_expr = U.minimize_and_clip(optimizer, loss, p_func_vars, grad_norm_clipping)
+        optimize_expr_nature = U.minimize_and_clip(optimizer_nature, loss_nature, p_nature_func_vars, grad_norm_clipping)
 
         # Create callable functions
         train = U.function(inputs=obs_ph_n + act_ph_n, outputs=loss, updates=[optimize_expr])
+        train_nature = U.function(inputs=obs_ph_n + act_ph_n + [perturbed_rewards], outputs=loss_nature, updates=[optimize_expr_nature])
         act = U.function(inputs=[obs_ph_n[p_index]], outputs=act_sample)
         # attention = U.function(inputs=[obs_ph_n[p_index]], outputs=[attn.good_attn, attn.adv_attn])
         p_values = U.function([obs_ph_n[p_index]], p)
@@ -86,124 +98,127 @@ def p_train(make_obs_ph_n, act_space_n, p_index, p_func, q_func, optimizer, grad
         target_p = p_func(p_input, int(act_pdtype_n[p_index].param_shape()[0]), scope="target_p_func", num_units=num_units)
         target_p_func_vars = U.scope_vars(U.absolute_scope_name("target_p_func"))
         update_target_p = make_update_exp(p_func_vars, target_p_func_vars)
+        target_p_nature = p_nature_func(p_nature_input, 1, scope="target_p_nature_func", num_units=num_units)
+        target_p_nature_func_vars = U.scope_vars(U.absolute_scope_name("target_p_nature_func"))
+        update_target_p_nature = make_update_exp(p_nature_func_vars, target_p_nature_func_vars)
 
         target_act_sample = act_pdtype_n[p_index].pdfromflat(target_p).sample()
         target_act = U.function(inputs=[obs_ph_n[p_index]], outputs=target_act_sample)
 
-        return act, train, update_target_p, {'p_values': p_values, 'target_act': target_act}
+        return act, train, train_nature, update_target_p, update_target_p_nature, {'p_values': p_values, 'target_act': target_act}
 
-def group_p_train(make_obs_ph_n, act_space_n, p_index, num_adversaries, p_func, q_func, optimizer, grad_norm_clipping=None, local_q_func=False, num_units=64, scope="trainer", reuse=None):
-    with tf.variable_scope(scope, reuse=reuse):
-        # create distribtuions
-        act_pdtype_n = [make_pdtype(act_space) for act_space in act_space_n]
+# def group_p_train(make_obs_ph_n, act_space_n, p_index, num_adversaries, p_func, q_func, optimizer, grad_norm_clipping=None, local_q_func=False, num_units=64, scope="trainer", reuse=None):
+#     with tf.variable_scope(scope, reuse=reuse):
+#         # create distribtuions
+#         act_pdtype_n = [make_pdtype(act_space) for act_space in act_space_n]
 
-        # set up placeholders for a group
-        obs_ph_n = make_obs_ph_n
-        n_agents = len(obs_ph_n)
+#         # set up placeholders for a group
+#         obs_ph_n = make_obs_ph_n
+#         n_agents = len(obs_ph_n)
 
-        if(p_index<num_adversaries):
-            act_ph_ns = [[act_pdtype_n[i].sample_placeholder([None], name="action"+str(n)+'_'+str(i)) for i in range(len(act_space_n))] for n in range(num_adversaries)]
-        else:
-            act_ph_ns = [[act_pdtype_n[i].sample_placeholder([None], name="action"+str(n)+'_'+str(i)) for i in range(len(act_space_n))] for n in range(n_agents-num_adversaries)]
-        act_ph_ns_flatten = list(chain.from_iterable(act_ph_ns))
+#         if(p_index<num_adversaries):
+#             act_ph_ns = [[act_pdtype_n[i].sample_placeholder([None], name="action"+str(n)+'_'+str(i)) for i in range(len(act_space_n))] for n in range(num_adversaries)]
+#         else:
+#             act_ph_ns = [[act_pdtype_n[i].sample_placeholder([None], name="action"+str(n)+'_'+str(i)) for i in range(len(act_space_n))] for n in range(n_agents-num_adversaries)]
+#         act_ph_ns_flatten = list(chain.from_iterable(act_ph_ns))
 
-        # p_input = obs_ph_n[p_index] # one obs for a certain p_index
-        # batchify obs for all agents in a group
-        if(p_index < num_adversaries): # adv
-            p_input = tf.concat(obs_ph_n[:num_adversaries], 1)
-            p_input = tf.reshape(p_input, [-1, p_input.shape[-1].value//num_adversaries])
-        else: # good agent
-            p_input = tf.concat(obs_ph_n[num_adversaries:], 1)
-            p_input = tf.reshape(p_input, [-1, p_input.shape[-1].value//(n_agents-num_adversaries)])
+#         # p_input = obs_ph_n[p_index] # one obs for a certain p_index
+#         # batchify obs for all agents in a group
+#         if(p_index < num_adversaries): # adv
+#             p_input = tf.concat(obs_ph_n[:num_adversaries], 1)
+#             p_input = tf.reshape(p_input, [-1, p_input.shape[-1].value//num_adversaries])
+#         else: # good agent
+#             p_input = tf.concat(obs_ph_n[num_adversaries:], 1)
+#             p_input = tf.reshape(p_input, [-1, p_input.shape[-1].value//(n_agents-num_adversaries)])
 
-        # get all actions from a group
-        p = p_func(p_input, int(act_pdtype_n[p_index].param_shape()[0]), scope="p_func", num_units=num_units)
-        p_func_vars = U.scope_vars(U.absolute_scope_name("p_func"))
+#         # get all actions from a group
+#         p = p_func(p_input, int(act_pdtype_n[p_index].param_shape()[0]), scope="p_func", num_units=num_units)
+#         p_func_vars = U.scope_vars(U.absolute_scope_name("p_func"))
 
-        # wrap parameters in distribution
-        # un-batchify actions from a group
-        if(p_index < num_adversaries):
-            p = tf.reshape(p, [-1, p.shape[-1]*num_adversaries])
-            ps = tf.split(p, num_or_size_splits=num_adversaries, axis=1)
-        else:
-            p = tf.reshape(p, [-1, p.shape[-1]*(n_agents-num_adversaries)])
-            ps = tf.split(p, num_or_size_splits=(n_agents-num_adversaries), axis=1)
+#         # wrap parameters in distribution
+#         # un-batchify actions from a group
+#         if(p_index < num_adversaries):
+#             p = tf.reshape(p, [-1, p.shape[-1]*num_adversaries])
+#             ps = tf.split(p, num_or_size_splits=num_adversaries, axis=1)
+#         else:
+#             p = tf.reshape(p, [-1, p.shape[-1]*(n_agents-num_adversaries)])
+#             ps = tf.split(p, num_or_size_splits=(n_agents-num_adversaries), axis=1)
 
-        # get probability distributions and action samples for a group
-        if(p_index < num_adversaries):
-            act_pds = [act_pdtype_n[i].pdfromflat(ps[i]) for i in range(num_adversaries)]
-            act_samples = [act_pds[i].sample() for i in range(num_adversaries)]
-        else:
-            act_pds = [act_pdtype_n[i].pdfromflat(ps[i-num_adversaries]) for i in range(num_adversaries, n_agents)]
-            act_samples = [act_pds[i].sample() for i in range(n_agents-num_adversaries)]
-        # act_pd = act_pdtype_n[p_index].pdfromflat(p)
-        # act_sample = act_pd.sample()
+#         # get probability distributions and action samples for a group
+#         if(p_index < num_adversaries):
+#             act_pds = [act_pdtype_n[i].pdfromflat(ps[i]) for i in range(num_adversaries)]
+#             act_samples = [act_pds[i].sample() for i in range(num_adversaries)]
+#         else:
+#             act_pds = [act_pdtype_n[i].pdfromflat(ps[i-num_adversaries]) for i in range(num_adversaries, n_agents)]
+#             act_samples = [act_pds[i].sample() for i in range(n_agents-num_adversaries)]
+#         # act_pd = act_pdtype_n[p_index].pdfromflat(p)
+#         # act_sample = act_pd.sample()
 
-        # p_reg = tf.reduce_mean(tf.square(act_pd.flatparam()))
-        # get average p_reg for a group
-        p_reg = tf.reduce_mean(tf.square(tf.concat([act_pd.flatparam() for act_pd in act_pds], -1)))
+#         # p_reg = tf.reduce_mean(tf.square(act_pd.flatparam()))
+#         # get average p_reg for a group
+#         p_reg = tf.reduce_mean(tf.square(tf.concat([act_pd.flatparam() for act_pd in act_pds], -1)))
 
 
-        # act_input_n = act_ph_n + []
-        act_input_ns = act_ph_ns
-        # act_input_n[p_index] = act_pd.sample()
-        # q_input = tf.concat(obs_ph_n + act_input_n, 1)
-        if(p_index < num_adversaries):
-            q_inputs = []
-            for i in range(num_adversaries):
-                act_input_ns[i][i] = act_pds[i].sample()
-                q_inputs.append(tf.concat(obs_ph_n + act_input_ns[i], 1))
-            # batchify q_input
-            q_input = tf.concat(q_inputs, 0)
-        else:
-            q_inputs = []
-            for i in range(n_agents-num_adversaries):
-                act_input_ns[i][i+num_adversaries] = act_pds[i].sample()
-                q_inputs.append(tf.concat(obs_ph_n + act_input_ns[i], 1))
-            # batchify q_input
-            q_input = tf.concat(q_inputs, 0)
+#         # act_input_n = act_ph_n + []
+#         act_input_ns = act_ph_ns
+#         # act_input_n[p_index] = act_pd.sample()
+#         # q_input = tf.concat(obs_ph_n + act_input_n, 1)
+#         if(p_index < num_adversaries):
+#             q_inputs = []
+#             for i in range(num_adversaries):
+#                 act_input_ns[i][i] = act_pds[i].sample()
+#                 q_inputs.append(tf.concat(obs_ph_n + act_input_ns[i], 1))
+#             # batchify q_input
+#             q_input = tf.concat(q_inputs, 0)
+#         else:
+#             q_inputs = []
+#             for i in range(n_agents-num_adversaries):
+#                 act_input_ns[i][i+num_adversaries] = act_pds[i].sample()
+#                 q_inputs.append(tf.concat(obs_ph_n + act_input_ns[i], 1))
+#             # batchify q_input
+#             q_input = tf.concat(q_inputs, 0)
 
-        # if local_q_func:
-        #     q_input = tf.concat([obs_ph_n[p_index], act_input_n[p_index]], 1)
+#         # if local_q_func:
+#         #     q_input = tf.concat([obs_ph_n[p_index], act_input_n[p_index]], 1)
 
-        # input group of q_input into q_func
-        q = q_func(q_input, 1, scope="q_func", reuse=True, num_units=num_units)[:,0]
-        pg_loss = -tf.reduce_mean(q)
+#         # input group of q_input into q_func
+#         q = q_func(q_input, 1, scope="q_func", reuse=True, num_units=num_units)[:,0]
+#         pg_loss = -tf.reduce_mean(q)
 
-        loss = pg_loss + p_reg * 1e-3
+#         loss = pg_loss + p_reg * 1e-3
 
-        optimize_expr = U.minimize_and_clip(optimizer, loss, p_func_vars, grad_norm_clipping)
+#         optimize_expr = U.minimize_and_clip(optimizer, loss, p_func_vars, grad_norm_clipping)
 
-        # Create callable functions
-        train = U.function(inputs=obs_ph_n + act_ph_ns_flatten, outputs=loss, updates=[optimize_expr])
-        if(p_index < num_adversaries):
-            # print([obs_ph_n[p_index]], act_samples[p_index])
-            act = U.function(inputs=[obs_ph_n[p_index]], outputs=act_samples[p_index])
-            p_values = U.function([obs_ph_n[p_index]], ps[p_index])
-        else:
-            # print([obs_ph_n[p_index]], act_samples[p_index-num_adversaries])
-            act = U.function(inputs=[obs_ph_n[p_index]], outputs=act_samples[p_index-num_adversaries])
-            p_values = U.function([obs_ph_n[p_index]], ps[p_index-num_adversaries])
+#         # Create callable functions
+#         train = U.function(inputs=obs_ph_n + act_ph_ns_flatten, outputs=loss, updates=[optimize_expr])
+#         if(p_index < num_adversaries):
+#             # print([obs_ph_n[p_index]], act_samples[p_index])
+#             act = U.function(inputs=[obs_ph_n[p_index]], outputs=act_samples[p_index])
+#             p_values = U.function([obs_ph_n[p_index]], ps[p_index])
+#         else:
+#             # print([obs_ph_n[p_index]], act_samples[p_index-num_adversaries])
+#             act = U.function(inputs=[obs_ph_n[p_index]], outputs=act_samples[p_index-num_adversaries])
+#             p_values = U.function([obs_ph_n[p_index]], ps[p_index-num_adversaries])
 
-        # target network for a group
-        if(p_index < num_adversaries):
-            p_input = tf.reshape(p_input, [-1, p_input.shape[-1]*num_adversaries])
-            p_inputs = tf.split(p_input, num_or_size_splits=num_adversaries, axis=1)
-            target_p = p_func(p_inputs[p_index], int(act_pdtype_n[p_index].param_shape()[0]), scope="target_p_func", num_units=num_units)
-        else:
-            p_input = tf.reshape(p_input, [-1, p_input.shape[-1]*(n_agents-num_adversaries)])
-            p_inputs = tf.split(p_input, num_or_size_splits=(n_agents-num_adversaries), axis=1)
-            target_p = p_func(p_inputs[p_index-num_adversaries], int(act_pdtype_n[p_index].param_shape()[0]), scope="target_p_func", num_units=num_units)
-        # target_p = p_func(p_input, int(act_pdtype_n[p_index].param_shape()[0]), scope="target_p_func", num_units=num_units)
-        target_p_func_vars = U.scope_vars(U.absolute_scope_name("target_p_func"))
-        update_target_p = make_update_exp(p_func_vars, target_p_func_vars)
+#         # target network for a group
+#         if(p_index < num_adversaries):
+#             p_input = tf.reshape(p_input, [-1, p_input.shape[-1]*num_adversaries])
+#             p_inputs = tf.split(p_input, num_or_size_splits=num_adversaries, axis=1)
+#             target_p = p_func(p_inputs[p_index], int(act_pdtype_n[p_index].param_shape()[0]), scope="target_p_func", num_units=num_units)
+#         else:
+#             p_input = tf.reshape(p_input, [-1, p_input.shape[-1]*(n_agents-num_adversaries)])
+#             p_inputs = tf.split(p_input, num_or_size_splits=(n_agents-num_adversaries), axis=1)
+#             target_p = p_func(p_inputs[p_index-num_adversaries], int(act_pdtype_n[p_index].param_shape()[0]), scope="target_p_func", num_units=num_units)
+#         # target_p = p_func(p_input, int(act_pdtype_n[p_index].param_shape()[0]), scope="target_p_func", num_units=num_units)
+#         target_p_func_vars = U.scope_vars(U.absolute_scope_name("target_p_func"))
+#         update_target_p = make_update_exp(p_func_vars, target_p_func_vars)
 
-        target_act_sample = act_pdtype_n[p_index].pdfromflat(target_p).sample()
-        target_act = U.function(inputs=[obs_ph_n[p_index]], outputs=target_act_sample)
+#         target_act_sample = act_pdtype_n[p_index].pdfromflat(target_p).sample()
+#         target_act = U.function(inputs=[obs_ph_n[p_index]], outputs=target_act_sample)
 
-        return act, train, update_target_p, {'p_values': p_values, 'target_act': target_act}
+#         return act, train, update_target_p, {'p_values': p_values, 'target_act': target_act}
 
-def q_train(make_obs_ph_n, act_space_n, q_index, q_func, nature_p_func, optimizer, grad_norm_clipping=None, local_q_func=False, scope="trainer", reuse=None, num_units=64):
+def q_train(make_obs_ph_n, act_space_n, q_index, q_func, p_nature_func, optimizer, grad_norm_clipping=None, local_q_func=False, scope="trainer", reuse=None, num_units=64):
     with tf.variable_scope(scope, reuse=reuse):
         # create distribtuions
         act_pdtype_n = [make_pdtype(act_space) for act_space in act_space_n]
@@ -224,7 +239,7 @@ def q_train(make_obs_ph_n, act_space_n, q_index, q_func, nature_p_func, optimize
 
         
         # R-MADDPG
-        rew = nature_p_func(q_input, 1, scope="nature_p_func", num_units=num_units)[:,0]
+        rew = p_nature_func(q_input, 1, scope="p_nature_func", num_units=num_units)[:,0]
 #         target_ph += rew
         
         q_loss = tf.reduce_mean(tf.square(q - target_ph - rew))
@@ -453,21 +468,23 @@ class RMADDPGAgentMicroSharedTrainer(AgentTrainer):
             act_space_n=act_space_n,
             q_index=agent_index,
             q_func=model_q,
-            nature_p_func=model_nature,
+            p_nature_func=model_nature,
             optimizer=tf.train.AdamOptimizer(learning_rate=args.lr),
             grad_norm_clipping=0.5,
             local_q_func=local_q_func,
             num_units=num_units,
             reuse=tf.AUTO_REUSE
         )
-        self.act, self.p_train, self.p_update, self.p_debug = p_train(
+        self.act, self.p_train, self.p_nature_train, self.p_update, self.p_nature_update, self.p_debug = p_train(
             scope=self.name,
             make_obs_ph_n=obs_ph_n,
             act_space_n=act_space_n,
             p_index=agent_index,
             p_func=model_p,
+            p_nature_func=model_nature,
             q_func=model_q,
             optimizer=tf.train.AdamOptimizer(learning_rate=args.lr),
+            optimizer_nature=tf.train.AdamOptimizer(learning_rate=args.lr_nature),
             grad_norm_clipping=0.5,
             local_q_func=local_q_func,
             num_units=num_units,
@@ -543,7 +560,7 @@ class RMADDPGAgentMicroSharedTrainer(AgentTrainer):
             # print(type(target_act_next_n))
             target_q_next = self.q_debug['target_q_values'](*(obs_next_n + target_act_next_n))
             # times.append(time.time() - tmp0)  # 62s
-            target_q += rew + self.gamma * (1.0 - done) * target_q_next
+            target_q += self.gamma * (1.0 - done) * target_q_next
         target_q /= num_sample
 
         
@@ -554,6 +571,7 @@ class RMADDPGAgentMicroSharedTrainer(AgentTrainer):
         # train p network
         # tmp0 = time.time()
         p_loss = self.p_train(*(obs_n + act_n))
+        p_loss_nature = self.p_nature_train(*(obs_n + act_n + [rew])) # rew or [rew]?
         # times.append(time.time() - tmp0)  # 166s
 
         # tmp0 = time.time()
@@ -563,6 +581,7 @@ class RMADDPGAgentMicroSharedTrainer(AgentTrainer):
         # tmp0 = time.time()
         self.q_update()
         # times.append(time.time() - tmp0)  # 10s
+        self.p_nature_update()
 
         # return [q_loss, p_loss, np.mean(target_q), np.mean(rew), np.mean(target_q_next), np.std(target_q), times]
         return []
